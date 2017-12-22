627223751340a1d9c25420fa85d2e6ea7e0d0d3a
JoaoGFarias <jgfd@cin.ufpe.br>
2017-12-21 22:47:15 -0300

    Setting project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='PyCPD',
+    version='0.0.1',
+    description='Joining the power of BDD and Data-Driven Testing in a lightweight command line tool written in Python',
+    long_description=readme,
+    author='Joao Farias',
+    author_email='jgfarias42@gmail.com',
+    url='https://github.com/JoaoGFarias/PyCPD',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
