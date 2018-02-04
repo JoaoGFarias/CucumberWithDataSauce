2239a25fd5b96cb02958221d60042a73a085b238
JoaoGFarias <jgfd@cin.ufpe.br>
2018-02-03 21:09:16 -0300

    Tests for basic file reading

--- sample/core.py
+++ sample/core.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import helpers
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+def foo():
+    return 'Hi'
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from .context import sample
+import sys
+import os
 
 import unittest
 
+class FeatureFileProcessorTestSuite(unittest.TestCase):
 
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
+    @classmethod
+    def setUpClass(self):
+        self.base_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+        self.simple_file_path = os.path.join(self.base_path, "test_data", "simple_file.feature")
+        self.simple_file = [
+            "Feature: Serve coffee",
+            "Scenario: Buy last coffee",
+            "Given there are 1 coffees left in the machine",
+            "And I have deposited 1",
+            "When I press the coffee button",
+            "Then I should be served a coffee"]
 
-    def test_absolute_truth_and_meaning(self):
-        assert True
+    def setUp(self):
+        self.file_processor = FeatureFileProcessor(self.basePath)
+
+    def test_can_read_feature_file(self):
+        returned_text = self.file_processor.read_file(self.simple_file_path)
+        assert self.file_processor.text == self.simple_file
+        assert returned_text == self.simple_file
 
 
 if __name__ == '__main__':
