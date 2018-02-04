085efd2bcb43923414787b000110415d0a263d6d
JoaoGFarias <jgfd@cin.ufpe.br>
2018-02-03 22:21:21 -0300

    Tests for Creating Scenarios

--- tests/test_data/simple_file.feature
+++ tests/test_data/simple_file.feature
@@ -5,3 +5,9 @@ Scenario: Buy last coffee
   And I have deposited 1$
   When I press the coffee button
   Then I should be served a coffee
+
+Scenario: Buy first coffee
+  Given there are 2 coffees left in the machine
+  And I have deposited 1$
+  When I press the coffee button
+  Then I should be served a coffee
--- tests/test_feature_file_processor.py
+++ tests/test_feature_file_processor.py
@@ -12,13 +12,21 @@ class FeatureFileProcessorTestSuite(unittest.TestCase):
     def setUpClass(self):
         self.base_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
         self.simple_file_path = os.path.join("test_data", "simple_file.feature")
-        self.simple_file = [
-            "Feature: Serve coffee",
+        self.feature_title = "Feature: Serve coffee"
+        self.first_scenario = [
             "Scenario: Buy last coffee",
             "Given there are 1 coffees left in the machine",
             "And I have deposited 1$",
             "When I press the coffee button",
             "Then I should be served a coffee"]
+        self.second_scenario = [
+            "Scenario: Buy first coffee",
+            "Given there are 2 coffees left in the machine",
+            "And I have deposited 1$",
+            "When I press the coffee button",
+            "Then I should be served a coffee"
+        ]
+        self.simple_file = [self.feature_title] + self.first_scenario + self.second_scenario
 
     def setUp(self):
         self.file_processor = FeatureFileProcessor(self.base_path)
@@ -27,6 +35,13 @@ class FeatureFileProcessorTestSuite(unittest.TestCase):
         returned_text = self.file_processor.read_file(self.simple_file_path)
         assert self.file_processor.file_text == self.simple_file
         assert returned_text == self.simple_file
+
+    def test_create_scenarios_objects(self):
+        self.file_processor.read_file(self.simple_file_path)
+        self.file_processor.create_scenarios()
+        assert self.file_processor.feature_title == self.feature_title
+        assert self.file_processor.scenarios[0] == self.first_scenario
+        assert self.file_processor.scenarios[1] == self.second_scenario
 
 if __name__ == '__main__':
     unittest.main()
