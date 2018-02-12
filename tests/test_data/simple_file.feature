542124229a9582a7a2041cc00bc5f896e9477b80
JoaoGFarias <jgfd@cin.ufpe.br>
2018-02-12 13:35:07 -0300

    Data file reading tests

--- tests/test_data/simple_file.feature
+++ tests/test_data/simple_file.feature
@@ -1,12 +1,12 @@
 Feature: Serve coffee
 
-Scenario: Buy last coffee
+Scenario: Buy last coffee{!scenario_1_file!}
   Given there are 1 coffees left in the machine
   And I have deposited 1$
   When I press the coffee button
   Then I should be served a coffee
 
-Scenario: Buy first coffee
+Scenario: Buy first coffee {!scenario_2_file!}
   Given there are 2 coffees left in the machine
   And I have deposited 1$
   When I press the coffee button
--- tests/test_feature_file_processor.py
+++ tests/test_feature_file_processor.py
@@ -5,6 +5,8 @@ from .context import FeatureFile
 from .context import Scenario
 import os
 
+from string import Template
+
 import unittest
 from nose2dep.core import depends
 
@@ -15,14 +17,17 @@ class FeatureFileProcessorTestSuite(unittest.TestCase):
         self.base_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
         self.simple_file_path = os.path.join("test_data", "simple_file.feature")
         self.feature_title = 'Feature: Serve coffee'
+        self.data_file_mark = Template("{!$file!}")
+        self.first_scenario_data_file = "scenario_1_file"
         self.first_scenario = [
-            "Scenario: Buy last coffee",
+            "Scenario: Buy last coffee" + self.data_file_mark.substitute(file=self.first_scenario_data_file),
             "Given there are 1 coffees left in the machine",
             "And I have deposited 1$",
             "When I press the coffee button",
             "Then I should be served a coffee"]
+        self.second_scenario_data_file = "scenario_2_file"
         self.second_scenario = [
-            "Scenario: Buy first coffee",
+            "Scenario: Buy first coffee " +  self.data_file_mark.substitute(file=self.second_scenario_data_file),
             "Given there are 2 coffees left in the machine",
             "And I have deposited 1$",
             "When I press the coffee button",
@@ -47,8 +52,10 @@ class FeatureFileProcessorTestSuite(unittest.TestCase):
         self.assertEqual(feature_file.number_of_scenarios(), 2)
         self.assertIsInstance(feature_file.scenario_at(1), Scenario)
         self.assertEqual(feature_file.scenario_at(1), self.first_scenario)
+        self.assertEqual(feature_file.scenario_at(1).data_file(), self.first_scenario_data_file)
         self.assertIsInstance(feature_file.scenario_at(2), Scenario)
         self.assertEqual(feature_file.scenario_at(2), self.second_scenario)
+        self.assertEqual(feature_file.scenario_at(2).data_file(), self.second_scenario_data_file)
 
 if __name__ == '__main__':
     unittest.main()
