0a686266220747a7d63b0fd5df73399260ff8f29
JoaoGFarias <jgfd@cin.ufpe.br>
2018-03-07 23:28:29 -0300

    Deletes content on target folder

--- FeatureFileProcessor/__init__.py
+++ FeatureFileProcessor/__init__.py
@@ -1,4 +1,5 @@
 from .core import FeatureFileProcessor
 from .file_models.feature_file import FeatureFile
 from .file_models.scenario import Scenario
+from .folder_processor.processor import FolderProcessor
 from .exceptions.no_data_file_exception import NoDataFileException
